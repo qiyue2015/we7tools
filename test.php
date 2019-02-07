11f7bcd0e957b4dd53ddee6d81e8d45c4b4c0602
白开水浇花 <fegnqiyue@gmail.com>
2019-02-07 12:04:55 +0800

    First commit

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by we7tools.
+ * User: fengqiyue
+ * Time: 2019/2/7 12:04 PM
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+use Hello\SayHello;
+
+echo SayHello::world();
