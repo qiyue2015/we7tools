3c12a651a3fec79dfcce045924538aa448950333
白开水浇花 <fegnqiyue@gmail.com>
2019-02-07 15:16:38 +0800

    update

---
+++ src/util.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by we7tools.
+ * User: fengqiyue
+ * Time: 2019/2/7 12:02 PM
+ */
+
+namespace LnApp;
+
+class Util
+{
+    /**
+     * API 返回正确信息
+     * @param number $errno 错误编号
+     * @param string $message 错误信息
+     */
+    public static function send_error($errno, $message = '')
+    {
+        @header('Content-type:application/json');
+        if (is_array($errno) && array_key_exists('errno', $errno)) {
+            $message = $errno['message'];
+            $errno = $errno['errno'];
+        }
+        $obj = [];
+        $obj['errno'] = intval($errno);
+        $obj['message'] = $message;
+        $obj = json_encode($obj, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        if ($_GET['callback']) {
+            $obj = $_GET['callback'] . '(' . $obj . ')';
+        }
+        die($obj);
+    }
+
+    /**
+     * API输出正确信息
+     * @param array $data
+     */
+    public static function send_result($data = array())
+    {
+        @header('Content-type:application/json');
+        $obj['errno'] = 0;
+        $obj['message'] = 'success';
+        $obj['data'] = !empty($data) && is_array($data) ? (array)$data : (object)$data;
+        $obj = json_encode($obj, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        if ($_GET['callback']) {
+            $obj = $_GET['callback'] . '(' . $obj . ')';
+        }
+        die($obj);
+    }
+}
--- test.php
+++ test.php
@@ -5,6 +5,10 @@
  * Time: 2019/2/7 12:04 PM
  */
 require_once __DIR__ . '/vendor/autoload.php';
-use Hello\SayHello;
+use LnApp\Util;
 
-echo SayHello::world();
+// $data = SayHello::send_error(-1, 'xxxx');
+// var_dump($data);
+
+// $data = SayHello::send_result(['username' => '叉叉', 'user_id' => 1000]);
+// var_dump($data);
